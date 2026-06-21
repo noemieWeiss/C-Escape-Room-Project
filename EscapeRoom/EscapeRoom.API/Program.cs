6203038b83283e306bcecfbf5cae378fa432df07
noemieWeiss <nw3996@gmail.com>
2026-06-21 16:10:34 +0300

    work on API with controller and SWAGER worksnow - can see get api\rooms

--- EscapeRoom/EscapeRoom.API/Program.cs
+++ EscapeRoom/EscapeRoom.API/Program.cs
@@ -1,23 +1,28 @@
+//using EscapeRoom.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// הגדרת החיבור למסד הנתונים MySQL
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();

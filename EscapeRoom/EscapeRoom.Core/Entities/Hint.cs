9a3a12aa1551bd7a2ba54bb795f40bdccfe31017
noemieWeiss <nw3996@gmail.com>
2026-06-21 16:28:12 +0300

    change name of class and update entities

--- EscapeRoom/EscapeRoom.Core/Entities/Hint.cs
+++ EscapeRoom/EscapeRoom.Core/Entities/Hint.cs
@@ -1,12 +1,18 @@
-﻿namespace EscapeRoom.Core.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EscapeRoom.Core.Entities
 {
     public class Hint
     {
+        [Key]
         public int Id { get; set; }
-        public string Description { get; set; }
-        public int PenaltyMinutes { get; set; }
+        public string Description { get; set; } = string.Empty;
 
-        public int EscapeRoomEntityId { get; set; }
-        public EscapeRoomEntity EscapeRoomEntity { get; set; }
+        // התעדכן ל-RoomId
+        public int RoomId { get; set; }
+
+        [ForeignKey("RoomId")]
+        public Room Room { get; set; } = null!;
     }
 }
---
+++ EscapeRoom/EscapeRoom.Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EscapeRoom.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EscapeRoom.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Room> Rooms { get; set; } // שינינו פה ל-Rooms!
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Hint> Hints { get; set; }
+    }
+}
